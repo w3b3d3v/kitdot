c4791c83f600f3c5ce96648ad82db360d628c750
Yan Luiz <yan@nomadbitcoin.xyz>
2025-09-23 00:58:20 -0500

    fix: contracts setup

--- templates/default/contracts/hardhat.config.ts
+++ templates/default/contracts/hardhat.config.ts
@@ -1,17 +1,23 @@
 import { HardhatUserConfig, vars } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@parity/hardhat-polkadot";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const privateKey = process.env.PRIVATE_KEY;
 if (!privateKey) {
-  console.warn("⚠️  WARNING: PRIVATE_KEY environment variable not found. Please set it in your .env file for network deployments.");
+  console.warn(
+    "⚠️  WARNING: PRIVATE_KEY environment variable not found. Please set it in your .env file for network deployments."
+  );
 }
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
-  //resolc: {
-  //  compilerSource: "npm"
-  //},
+  resolc: {
+    compilerSource: "npm",
+    settings: {},
+  },
   networks: {
     hardhat: {
       // polkavm: true,
@@ -26,7 +32,7 @@ const config: HardhatUserConfig = {
     localhost: {
       url: "http://127.0.0.1:8545/",
     },
-    polkadotHubTestnet: {
+    passetHubTestnet: {
       polkavm: true,
       url: "https://testnet-passet-hub-eth-rpc.polkadot.io",
       accounts: privateKey ? [privateKey] : [],
